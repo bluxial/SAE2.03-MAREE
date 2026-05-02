c005beceb41218ab3570425d91b7e3e907e2def4
Maree Jules <maree2@mmi-dev.unilim.fr>
2026-05-02 18:38:20 +0200

    Réparation URL cassé

--- admin/data/dataMovie.js
+++ admin/data/dataMovie.js
@@ -1,25 +1,30 @@
-let HOST_URL = window.location.origin;
+let HOST_URL = "https://mmi.unilim.fr/~maree2/SAE2.03-MAREE";
 
 let DataMovie = {};
 
-DataMovie.requestMovies = async function() {
-    let answer = await fetch(HOST_URL + "/server/script.php?todo=readmovies");
-    let data = await answer.json();
-    return data;
+DataMovie.requestMovies = async function () {
+  let answer = await fetch(HOST_URL + "/server/script.php?todo=readmovies");
+  let data = await answer.json();
+  return data;
 };
 
-DataMovie.add = async function(fdata) {
-    let config = { method: "POST", body: fdata };
-    let answer = await fetch(HOST_URL + "/server/script.php?todo=addmovie", config);
-    let data = await answer.json();
-    return data;
+DataMovie.add = async function (fdata) {
+  let config = { method: "POST", body: fdata };
+  let answer = await fetch(
+    HOST_URL + "/server/script.php?todo=addmovie",
+    config,
+  );
+  let data = await answer.json();
+  return data;
 };
 
-DataMovie.requestMovieDetails = async function(id) {
-    // on récupère les détails du film
-    let answer = await fetch(HOST_URL + "/server/script.php?todo=readmoviedetail&id=" + id);
-    let data = await answer.json();
-    return data;
+DataMovie.requestMovieDetails = async function (id) {
+  // on récupère les détails du film
+  let answer = await fetch(
+    HOST_URL + "/server/script.php?todo=readmoviedetail&id=" + id,
+  );
+  let data = await answer.json();
+  return data;
 };
 
 export { DataMovie };
--- app/data/dataFavorite.js
+++ app/data/dataFavorite.js
@@ -1,23 +1,25 @@
-let HOST_URL = window.location.origin;
+let HOST_URL = "https://mmi.unilim.fr/~maree2/SAE2.03-MAREE";
 
 let DataFavorite = {};
 
-DataFavorite.add = async function(id_profile, id_movie) {
-    let fdata = new FormData();
-    fdata.append("id_profile", id_profile);
-    fdata.append("id_movie", id_movie);
-    let answer = await fetch(HOST_URL + "/server/script.php?todo=addfavorite", {
-        method: "POST",
-        body: fdata
-    });
-    let data = await answer.json();
-    return data;
+DataFavorite.add = async function (id_profile, id_movie) {
+  let fdata = new FormData();
+  fdata.append("id_profile", id_profile);
+  fdata.append("id_movie", id_movie);
+  let answer = await fetch(HOST_URL + "/server/script.php?todo=addfavorite", {
+    method: "POST",
+    body: fdata,
+  });
+  let data = await answer.json();
+  return data;
 };
 
-DataFavorite.read = async function(id_profile) {
-    let answer = await fetch(HOST_URL + "/server/script.php?todo=readfavorites&id_profile=" + id_profile);
-    let data = await answer.json();
-    return data;
+DataFavorite.read = async function (id_profile) {
+  let answer = await fetch(
+    HOST_URL + "/server/script.php?todo=readfavorites&id_profile=" + id_profile,
+  );
+  let data = await answer.json();
+  return data;
 };
 
 export { DataFavorite };
